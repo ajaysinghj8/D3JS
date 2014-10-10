1daaff05fd5b100f66b5639a4d655effbcb0b8af
Ajay Singh <meajaysingh@hotmail.com>
2014-10-10 22:17:59 +0530

    Initial commit

---
+++ 1/script.js
@@ -0,0 +1,8 @@
+(function(document) {
+    var data = [4, 8, 14, 13, 24, 67];
+    var div = document.createElement('div');
+    div.innerHTML = 'Hello, World!';
+    document.body.appendChild(div);
+
+
+})(window.document);
