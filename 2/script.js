740608219597d78acab48f283b8f27601a221e62
Ajay Singh <meajaysingh@hotmail.com>
2014-10-11 13:14:20 +0530

    labels in pie charts

--- 2/script.js
+++ 2/script.js
@@ -29,10 +29,28 @@
         .append('g')
         .attr('transform', 'translate(' + (width - radius) + ',' + (height - radius) + ')')
         .selectAll('path').data(pie(piedata))
-        .enter().append('path')
+        .enter().append('g')
+        .attr('class', 'slice')
+
+    var slices = d3.selectAll('g.slice')
+        .append('path')
         .attr('fill', function(d, i) {
             return colors(i);
         })
         .attr('d', arc)
+
+    var text = d3.selectAll('g.slice')
+        .append('text')
+        .text(function(d, i) {
+            return d.data.label;
+        })
+        .attr('text-anchor', 'middle')
+        .attr('fill', 'white')
+        .attr('transform', function(d) {
+            d.innerRadius = 0;
+            d.outerRadius = radius;
+            return 'translate('+ arc.centroid(d)+')';
+        })
+
 
 })(window.d3);
