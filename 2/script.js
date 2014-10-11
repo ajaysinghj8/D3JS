1a2e608c920fb89846afb73153f4d6b5dea2f2b7
Ajay Singh <meajaysingh@hotmail.com>
2014-10-11 12:31:30 +0530

    Basic pie

--- 2/script.js
+++ 2/script.js
@@ -1,4 +1,38 @@
 (function(d3) {
 
+    var width = 400,
+        height = 400,
+        radius = 200,
+        colors = d3.scale.category20c();
+    var piedata = [{
+        label: 'Ajay',
+        value: 90
+    }, {
+        label: 'Anish',
+        value: 50
+    }, {
+        label: 'Akhil',
+        value: 50
+    }];
+
+
+    var pie = d3.layout.pie()
+        .value(function(d) {
+            return d.value;
+        })
+    var arc = d3.svg.arc()
+        .outerRadius(radius)
+
+    var myPie = d3.select('#chart').append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' + (width - radius) + ',' + (height - radius) + ')')
+        .selectAll('path').data(pie(piedata))
+        .enter().append('path')
+        .attr('fill', function(d, i) {
+            return colors(i);
+        })
+        .attr('d', arc)
 
 })(window.d3);
