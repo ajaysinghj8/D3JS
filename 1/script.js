f75694ea9ad138629da17a402a820925cf7e051f
Ajay Singh <meajaysingh@hotmail.com>
2014-10-11 10:27:40 +0530

    events to action on barcharts

--- 1/script.js
+++ 1/script.js
@@ -1,12 +1,12 @@
 (function(d3) {
 
     var bardata = [];
-     for (var i =0; i <100; i++) {
-        bardata.push(Math.random()*30)
-     };
+    for (var i = 0; i < 100; i++) {
+        bardata.push(Math.random() * 30)
+    };
     var height = 400,
         width = 600;
-
+var tempcolor;
     var colors = d3.scale.linear()
         .domain([0, bardata.length * .33, bardata.length * .66, bardata.length])
         .range(['#FFB832', '#C61C6F', '#268BD@', '#85992']);
@@ -21,7 +21,7 @@
     d3.select('#chart').append('svg')
         .attr('width', width)
         .attr('height', height)
-//        .style('background', '#C9D7D6')
+        //        .style('background', '#C9D7D6')
         .selectAll('rect').data(bardata)
         .enter().append('rect')
         .style('fill', function(d, i) {
@@ -36,6 +36,19 @@
         })
         .attr('y', function(d) {
             return height - yScale(d);
+        })
+        //events
+        .on('mouseover', function(d) {
+            tempcolor = this.style.fill;
+            d3.select(this)
+            .style('opacity',0.5)
+            .style('fill','Whiite')
+        })
+
+        .on('mouseout', function(d) {
+            d3.select(this)
+            .style('opacity',1)
+            .style('fill', tempcolor);
         })
 
 })(window.d3);
