6f53e8a6aae5dcbb5092127ba5f317ceaad64ed1
Ajay Singh <meajaysingh@hotmail.com>
2014-10-12 20:55:07 +0530

    circle using d3

--- 5/script.js
+++ 5/script.js
@@ -1,6 +1,14 @@
 (function(d3) {
 
-
+    d3.select('body')
+        .append('svg')
+        .attr('width', 100)
+        .attr('height', 100)
+        .append('circle')
+        .attr('cx', 50)
+        .attr('cy', 50)
+        .attr('r', 25)
+        .style('fill', 'pink');
 
 
 })(window.d3);
