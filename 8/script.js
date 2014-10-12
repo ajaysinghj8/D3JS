a346a062915f666787c4758f3668ab52637a6918
Ajay Singh <meajaysingh@hotmail.com>
2014-10-12 23:23:03 +0530

    color picker

--- 8/script.js
+++ 8/script.js
@@ -3,12 +3,16 @@
     var w = 300,
         h = 100,
         padding = 2;
-    var dataset = [5, 10, 15, 20, 25];
+    var dataset = [5, 10, 15, 20, 25,11,18,9];
 
     var svg = d3.select('body')
         .append('svg')
         .attr('width', w)
         .attr('height', h);
+        function  colorPicker (v) {
+            if(v<=20) { return '#666666';}
+            else if(v>20) { return '#FF0033'}
+        }
 
     svg.selectAll('rect')
         .data(dataset)
@@ -26,7 +30,7 @@
                 return d * 4;
             },
             fill: function(d) {
-                return 'rgb(0,' + (d * 10) + ',0)';
+                return colorPicker(d);
             }
         });
 
