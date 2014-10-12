d12d1e7ea3716cbbe23c6be6b92c7a301baf6b6f
Ajay Singh <meajaysingh@hotmail.com>
2014-10-12 22:25:55 +0530

    Simple bar chart

--- 7/script.js
+++ 7/script.js
@@ -1,4 +1,26 @@
 (function(d3) {
 
+    var w = 300,
+        h = 150,
+        padding = 2;
+    var dataset = [125, 20, 25, 50, 135];
+    var svg = d3.select('body')
+        .append('svg')
+        .attr('width', w)
+        .attr('height', h);
+    svg.selectAll('rect')
+        .data(dataset)
+        .enter()
+        .append('rect')
+        .attr('x', function(d, i) {
+            return i * (w / dataset.length);
+        })
+        .attr('y', function(d) {
+            return h - d;
+        })
+        .attr('width', w / dataset.length - padding)
+        .attr('height', function(d, i) {
+            return d;
+        })
 
 })(window.d3);
