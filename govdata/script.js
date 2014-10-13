77df6a5ab246fc0e543f38f5b215ef27eed70df6
Ajay Singh <meajaysingh@hotmail.com>
2014-10-13 23:05:01 +0530

    using real world data

---
+++ govdata/script.js
@@ -0,0 +1,33 @@
+(function(d3) {
+
+    var h = 100,
+        w = 500;
+    d3.
+
+    // var lineFun = d3.svg.line()
+    //     .x(function(d) {
+    //         return d.months * 2;
+    //     })
+    //     .y(function(d) {
+    //         return d.sales;
+    //     })
+    //     .interpolate('linear');
+
+    // var svg = d3.select('body')
+    //     .append('svg')
+    //     .attr({
+    //         width: w,
+    //         height: h
+    //     });
+
+    // var viz = svg.append('path')
+    // .attr({
+    //     d: lineFun(monthlySales),
+    //     'stroke': 'purple',
+    //     'stroke-width':2,
+    //     'fill':'none'
+
+    // })
+
+
+})(window.d3);
